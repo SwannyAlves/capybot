e5cb7033a28123feba6f07ec15119b9da4025973
leticiafrontend <leticia.alesilva@outlook.com>
2025-07-22 20:40:56 -0300

    chore: setup typescript

--- eslint.config.js
+++ eslint.config.js
@@ -1,38 +1,47 @@
-const js = require('@eslint/js');
-const globals = require('globals');
+import js from '@eslint/js';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import globals from 'globals';
 
-module.exports = [
+export default [
   js.configs.recommended,
   {
+    files: ['**/*.ts'],
     languageOptions: {
-      ecmaVersion: 2022,
-      sourceType: 'commonjs',
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 2022,
+        sourceType: 'module',
+        project: './tsconfig.json',
+      },
       globals: {
         ...globals.node,
       },
     },
-    rules: {
-      'no-unused-vars': ['error', { 
-        argsIgnorePattern: '^_',
-        varsIgnorePattern: '^_' 
-      }],
-      'no-console': 'off',
-      'no-undef': 'error',
-      
-      'prefer-const': 'warn',
-      'no-var': 'warn',
-      'eqeqeq': ['warn', 'always'],
-
-      'quotes': 'off',
-      'semi': 'off',
-      'indent': 'off',
-      'comma-dangle': 'off',
+    plugins: {
+      '@typescript-eslint': tsPlugin,
     },
-    ignores: [
-      'node_modules/',
-      'auth_info/',
-      'temp/',
-      '*.log',
-    ],
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+        },
+      ],
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-non-null-assertion': 'warn',
+      '@typescript-eslint/prefer-nullish-coalescing': 'warn',
+      '@typescript-eslint/prefer-optional-chain': 'warn',
+
+      'no-unused-vars': 'off',
+      'no-console': 'off',
+      'prefer-const': 'warn',
+      'no-var': 'error',
+      eqeqeq: ['warn', 'always'],
+    },
+    ignores: ['node_modules/', 'dist/', 'auth_info/', 'temp/', '*.log', '*.js'],
   },
 ];
